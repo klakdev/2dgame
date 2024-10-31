b20c5805e64a083b224202b95c7f6e3b3f0f5248
Yaki Klein <yaki.klein@intel.com>
2024-10-31 15:57:20 +0200

    Update the names of variables
    a nd b are not good names for variables, instead I've changed the names to first and second.

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -1,9 +1,8 @@
 package main;
 
 public class Calculator {
-
-    public static double add(double a, double b) {
-        return a + b;
+    public static double add(double first, double second) {
+        return  first + second;
     }
 
     public static double multiply(double a, double b) {
